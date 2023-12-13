5939fa04dd64e0993ab044556a8f96770d86de71
JakubWaj <jw303269@student.polsl.pl>
2023-12-13 18:19:53 +0100

    Many to many relationship mode between user and group

---
+++ api/Domain/Entity/GroupMember.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entity;
+
+public class GroupMember
+{
+    public int Id { get; set; }
+    public Guid GroupId { get; set; }
+    public Group Group { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+}
