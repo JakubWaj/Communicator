6ccd49978166d4ac05fd9064c6b6d89b31577509
JakubWaj <jw303269@student.polsl.pl>
2023-12-13 19:18:28 +0100

    Created dbcontext for application

---
+++ api/Infrastructure/DAL/ComContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Group = System.Text.RegularExpressions.Group;
+
+namespace Infrastructure.DAL;
+
+public class ComContext : DbContext
+{
+    public ComContext(DbContextOptions<ComContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Group> Groups { get; set; }    
+    public DbSet<GroupMember> GroupMembers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ComContext).Assembly);
+    }
+}
