abaa9a125d189861b1a8fc07667e9ca97d1edd86
JakubWaj <jw303269@student.polsl.pl>
2023-12-13 18:18:48 +0100

    Message database model

---
+++ api/Domain/Entity/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entity;
+
+public class Message
+{
+    public Guid Id { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid SenderId { get; set; }
+    public User Sender { get; set; }
+    public Guid ReceiverId { get; set; }
+    public User Receiver { get; set; }
+}
