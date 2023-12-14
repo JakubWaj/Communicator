6376d58a20b5dc4b03992661f3eaa85c5ebac58d
JakubWaj <jw303269@student.polsl.pl>
2023-12-14 18:13:07 +0100

    Authentication and password hashing

---
+++ api/Infrastructure/DAL/Security/PasswordManager.cs
@@ -0,0 +1,19 @@
+﻿using Application.Security;
+using Domain.Entity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.DAL.Security;
+
+public class PasswordManager : IPasswordManager
+{
+    private readonly IPasswordHasher<User> _passwordHasher;
+
+    public PasswordManager(IPasswordHasher<User> passwordHasher)
+    {
+        _passwordHasher = passwordHasher;
+    }
+
+    public string Secure(string password) => _passwordHasher.HashPassword(null, password);
+
+    public bool Validate(string password, string securedPassword) => _passwordHasher.VerifyHashedPassword(null, securedPassword, password) == PasswordVerificationResult.Success;
+}
--- api/Infrastructure/Extensions.cs
+++ api/Infrastructure/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.DAL;
+﻿using Infrastructure.Auth;
+using Infrastructure.DAL;
+using Infrastructure.DAL.Security;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +11,8 @@ public static class Extensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddPostgres(configuration);
+        services.AddAuth(configuration);
+        services.AddSecurity();
         return services;
     }
     public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
