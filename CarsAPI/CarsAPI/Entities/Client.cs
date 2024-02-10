a679404f353cb3d6ca5578afb2a481c56db13244
JakubWaj <jw303269@student.polsl.pl>
2024-02-10 09:39:52 +0100

    Model created

---
+++ CarsAPI/CarsAPI/Entities/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsAPI.Entities;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public virtual ICollection<Rental> Rentals { get; set; }
+}
---
+++ CarsAPI/CarsAPI/Entities/Rental.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsAPI.Entities;
+
+public class Rental
+{
+    public int CarId { get; set; }
+    public int Client { get; set; }
+    public DateTime RentalDate { get; set; }
+    public DateTime ReturnDate { get; set; }
+    public double TotalCost { get; set; }
+}
